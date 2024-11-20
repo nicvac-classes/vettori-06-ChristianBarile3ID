371f7458829d3b6c5256f1bb7222ced9f40f4715
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2024-11-20 09:52:40 +0000

    scambiati i for con i while

--- main.cpp
+++ main.cpp
@@ -6,27 +6,32 @@ using namespace std;
 int main() {
     srand(time(0));
     int nAtleti, i, iMin;
-    double tempoMigliore;
     cout << "Quanti atleti partecipano a questa competizione?" << endl;
     cin >> nAtleti;
-    vector<string>nomi(nAtleti);
-    vector<string>nazionali(nAtleti);
-    vector<float>tempi(nAtleti);
-    for (i = 0; i <= nomi.size() - 1; i++) {
+    vector<string> nomi(nAtleti);
+    vector<string> nazionali(nAtleti);
+    vector<float> tempi(nAtleti);
+    i = 0;
+    while (i<nomi.size()) {
         cout << "Come si chiama l'atleta numero " << i + 1 << "?" << endl;
         cin >> nomi[i];
         cout << "Da quale nazione proviene?" << endl;
         cin >> nazionali[i];
-        tempi[i] = (double) (rand() % 1300) / 100 + 5;
+        tempi[i] = (rand() % 1300) / 100 + 5;
+        i++;
     }
-    for (i = 0; i <= nomi.size() - 1; i++) {
+    i = 0;
+    while (i<nomi.size()) {
         cout << "L'atleta numero " << i + 1 << " si chiama " << nomi[i] << " proviene dalla nazione " << nazionali[i] << " ed ha finito la gara in " << tempi[i] << " secondi" << endl;
+        i++;
     }
+    i = 0;
     iMin = 0;
-    for (i = 0; i <= tempi.size() - 1; i++) {
+    while (i<tempi.size()) {
         if (tempi[i] < tempi[iMin]) {
             iMin = i;
         }
+        i++;
     }
-    cout << "Il vincitore della gara è " << nomi[iMin] << " proveniente dal " << nazionali[iMin] << " che ha finito la gara in " << tempi[iMin] << " secondi" << endl;
+    cout << "Il vincitore della gara è " << nomi[iMin] << " proveniente dalla nazione " << nazionali[iMin] << " che ha finito la gara in " << tempi[iMin] << " secondi" << endl;
 }
